f0d34a9cd5ccca1a43f2442e3793e723f9babc89
ahmed-com <ahmed0grwan@gmail.com>
2021-12-20 16:18:39 +0200

    implemented the is-owner policy

--- src/api/deal/controllers/deal.js
+++ src/api/deal/controllers/deal.js
@@ -1,9 +1,26 @@
-'use strict';
+"use strict";
 
 /**
  *  deal controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::deal.deal');
+module.exports = createCoreController("api::deal.deal", () => ({
+  async create(ctx) {
+    ctx.request.body.owner = ctx.state.user.id;
+
+    const response = await super.create(ctx);
+
+    return response;
+  },
+
+  async update(ctx) {
+    const response = await super.update(ctx);
+
+    if (response.body.id !== ctx.state.user.id)
+      return ctx.unauthorized(`You can't update this entry`);
+
+    return response;
+  },
+}));
