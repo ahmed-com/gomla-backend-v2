97ab3b7f4d12db07470d35ca6202f99dc1ba26ea
ahmed-com <ahmed0grwan@gmail.com>
2021-12-27 12:27:12 +0200

    added an intermediary domain

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('', 'https://6f59-197-42-84-114.ngrok.io'),
+  url: env('', 'https://gomla.club'),
 });
